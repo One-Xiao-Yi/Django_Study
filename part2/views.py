817fec8238b7eac5b3c968344e6b19e763fac409
One-Xiao-Yi <One-Xiao-Yi@users.noreply.github.com>
2019-07-14 18:46:41 +0800

    第二部分，添加静态资源，并完成CURD操作

--- part2/views.py
+++ part2/views.py
@@ -4,11 +4,15 @@ from django.http import HttpResponse,JsonResponse
 import time,json
 
 
+# 为用户界面提供初始数据
+# render用于返回xml文件，三个参数中，第一个为请求，第二个为xml文件名，第三个为向该请求传递的数据
 def ini_ward(request):
     ward_list = Wards.objects.all()
     return render(request,"index.html",{"ward_list":ward_list})
 
 
+# 添加数据操作
+# JsonResponse用于返回json格式数据
 def add_ward(request):
     ward = Wards()
     ward.ward_id = 108
@@ -19,6 +23,7 @@ def add_ward(request):
     return JsonResponse({'msg': "插入成功"})
 
 
+# 删除数据操作
 def del_ward(request):
     ward_id = request.GET.get("ward_id")
     ward = Wards.objects.get(ward_id=ward_id)
@@ -26,6 +31,7 @@ def del_ward(request):
     return JsonResponse({'msg': "删除成功"})
 
 
+# 更新数据操作
 def update_ward(request):
     ward = Wards(**json.loads(request.body))
     ward.save()
